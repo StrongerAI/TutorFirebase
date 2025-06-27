b5ad0d484ba3192baa461225308408d4aad2f683
Khurram Mansoor <khurram.mansur@gmail.com>
2025-06-27 10:56:07 +0000

    when i have logged in and then i press the tutortrack logo button on the

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -17,7 +17,7 @@ export function AppHeader() {
   const pathname = usePathname();
 
   const navItems: NavItem[] = role === 'student' ? STUDENT_NAV_ITEMS : TEACHER_NAV_ITEMS;
-  const homePath = '/';
+  const homePath = role ? `/${role}/dashboard` : '/';
 
   const NavLink = ({ item }: { item: NavItem }) => {
     const isActive = item.href && pathname.startsWith(item.href);
