f95e6aa94ede6a0ea1e5d75b29f438f015e70708
Khurram Mansoor <khurram.mansur@gmail.com>
2025-06-27 07:20:36 +0000

    you might want to do something with this

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDdB_4nMoiFAHBiVOmdiStc04NEJGq4rb4",
+  authDomain: "tutortrackai-spjni.firebaseapp.com",
+  projectId: "tutortrackai-spjni",
+  storageBucket: "tutortrackai-spjni.appspot.com",
+  messagingSenderId: "840717533673",
+  appId: "1:840717533673:web:5ab2fa9fe9212167b50b78"
 };
 
 // Initialize Firebase
